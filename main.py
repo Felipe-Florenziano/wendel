9b41357f5d3c3c26e1c5e80e9e7c4bf10c3ca680
Felipe-Florenziano <150644020+Felipe-Florenziano@users.noreply.github.com>
2024-01-18 19:54:57 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+
+caminho1 = '1.jpg'
+caminho2 = '2.png'
+caminho3 = '3.png'
+caminho4 = '4.png'
+caminho5 = '5.png'
+caminho6 = '6.png'
+caminho7 = '7.png'
+caminho8 = '8.png'
+caminho9 = '9.png'
+caminho10 = '10.png'
+caminho11 = '11.png'
+caminho12 = '12.png'
+caminho13 = '13.png'
+caminho14 = '14.png'
+caminho15 = '15.png'
+caminho16 = '16.png'
+caminho17 = '17.png'
+caminho18 = '18.png'
+
+st.image(caminho1)
+st.image(caminho2)
+st.image(caminho3)
+st.image(caminho4)
+st.image(caminho5)
+st.image(caminho6)
+st.image(caminho7)
+st.image(caminho8)
+st.image(caminho9)
+st.image(caminho10)
+st.image(caminho11)
+st.image(caminho12)
+st.image(caminho13)
+st.image(caminho14)
+st.image(caminho15)
+st.image(caminho16)
+st.image(caminho17)
+st.image(caminho18)
+
